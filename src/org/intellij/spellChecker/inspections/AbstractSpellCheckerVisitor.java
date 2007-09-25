8bbc0caf932fc7a69e0ba31870dcfd42fb45f54e
sergiy.dubovik <sergiy.dubovik@788ae795-663a-0410-9dc1-159affa97516>
2007-09-25 21:20:41 +0000

    - added spelling in xml (html) text and attributes.

--- src/org/intellij/spellChecker/inspections/AbstractSpellCheckerVisitor.java
+++ src/org/intellij/spellChecker/inspections/AbstractSpellCheckerVisitor.java
@@ -20,8 +20,13 @@ import com.intellij.codeInspection.ProblemDescriptor;
 import com.intellij.openapi.util.TextRange;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiRecursiveElementVisitor;
+import org.intellij.spellChecker.util.Strings;
+import org.jetbrains.annotations.NonNls;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Base visitor support for spelling.
@@ -30,6 +35,15 @@ import java.util.List;
  */
 public abstract class AbstractSpellCheckerVisitor extends PsiRecursiveElementVisitor implements SpellCheckerVisitor {
     protected InspectionManager inspectionManager;
+    @NonNls
+    private static final Pattern NON_SPACE = Pattern.compile("\\S+");
+    @NonNls
+    private static final Pattern WORD = Pattern.compile("\\b\\p{Alpha}+\\b");
+    @NonNls
+    private static final Pattern URL = Pattern.compile("(https?|ftp|mailto)\\:\\/\\/");
+    @NonNls
+    private static final Pattern COMPLEX = Pattern.compile("(\\.[^\\.]+)|([@_]+)");
+    private List<ProblemDescriptor> problems;
 
     protected AbstractSpellCheckerVisitor(InspectionManager inspectionManager) {
         this.inspectionManager = inspectionManager;
@@ -37,5 +51,44 @@ public abstract class AbstractSpellCheckerVisitor extends PsiRecursiveElementVis
 
     protected List<ProblemDescriptor> inspect(PsiElement element, TextRange textRange, String word) {
         return SpellCheckerInspector.inspectWithChangeTo(inspectionManager, element, textRange, word);
+    }
+
+    protected void forEachWord(PsiElement element, String text) {
+        // Create a pattern to match breaks
+        Matcher matcher = NON_SPACE.matcher(text);
+        while (matcher.find()) {
+            visitNonSpace(element, matcher.start(), matcher.end());
+        }
+    }
+
+    private void visitNonSpace(PsiElement element, int start, int end) {
+        if (end - start > 1) {
+            String text = element.getText().substring(start, end);
+            if (!URL.matcher(text).find() && !COMPLEX.matcher(text).find()) {
+                Matcher matcher = WORD.matcher(text);
+                while (matcher.find()) {
+                    visitWord(element, start + matcher.start(), start + matcher.end());
+                }
+            }
+        }
+    }
+
+    private void visitWord(PsiElement element, int start, int end) {
+        if (end - start > 1) {
+            String word = element.getText().substring(start, end);
+            if (!Strings.isMixedCase(word)) {
+                List<ProblemDescriptor> list = inspect(element, new TextRange(start, end), word);
+                if (list.size() > 0) {
+                    if (problems == null) {
+                        problems = new ArrayList<ProblemDescriptor>(list.size());
+                    }
+                    problems.addAll(list);
+                }
+            }
+        }
+    }
+
+    public ProblemDescriptor[] getProblems() {
+        return problems != null ? problems.toArray(new ProblemDescriptor[problems.size()]) : null;
     }
 }
