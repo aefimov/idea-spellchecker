a61b7681bfa7625eeb033b0f0c6cdfca1277e6b6
sergiy.dubovik <sergiy.dubovik@788ae795-663a-0410-9dc1-159affa97516>
2007-09-26 14:52:52 +0000

    - added proprties value spell checking

---
+++ trunk/src/org/intellij/spellChecker/inspections/AdvancedPropertiesSpellingInspection.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2007 Sergiy Dubovik, Alexey Efimov
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.intellij.spellChecker.inspections;
+
+import com.intellij.codeInspection.InspectionManager;
+import com.intellij.codeInspection.LocalInspectionTool;
+import com.intellij.codeInspection.ProblemDescriptor;
+import com.intellij.lang.Language;
+import com.intellij.psi.PsiFile;
+import org.intellij.spellChecker.util.SpellCheckerBundle;
+import org.jetbrains.annotations.Nls;
+import org.jetbrains.annotations.NonNls;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+/**
+ * Checks strings in property files.
+ *
+ * @author Sergiy Dubovik
+ */
+public class AdvancedPropertiesSpellingInspection extends LocalInspectionTool {
+
+    @Nls
+    @NotNull
+    public String getGroupDisplayName() {
+        return SpellCheckerBundle.message("spelling");
+    }
+
+    @Nls
+    @NotNull
+    public String getDisplayName() {
+        return SpellCheckerBundle.message("advanced.properties.spelling.inspection");
+    }
+
+    @NonNls
+    @NotNull
+    public String getShortName() {
+        return "AdvancedPropertiesSpellingInspection";
+    }
+
+    @Nullable
+    public ProblemDescriptor[] checkFile(@NotNull PsiFile file, @NotNull InspectionManager manager, boolean isOnTheFly) {
+        Language language = file.getLanguage();
+        if (language.getID().equals("Properties")) {
+            AbstractSpellCheckerVisitor visitor = new AdvancedPropertiesSpellingVisitor(manager);
+            file.accept(visitor);
+            return visitor.getProblems();
+        }
+
+        return null;
+    }
+}
