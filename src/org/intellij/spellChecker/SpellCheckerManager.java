2391bdb6c681419691d6e9b28e55e53932ed77b6
aefimov.box <aefimov.box@788ae795-663a-0410-9dc1-159affa97516>
2007-10-08 13:36:27 +0000

    Revert severity level to INFO and fix ignore word bug

--- src/org/intellij/spellChecker/SpellCheckerManager.java
+++ src/org/intellij/spellChecker/SpellCheckerManager.java
@@ -16,22 +16,12 @@
 package org.intellij.spellChecker;
 
 import com.intellij.codeHighlighting.HighlightDisplayLevel;
-import com.intellij.codeInsight.daemon.impl.HighlightInfoType;
-import com.intellij.codeInsight.daemon.impl.SeverityRegistrar;
 import com.intellij.codeInspection.InspectionToolProvider;
-import com.intellij.lang.annotation.HighlightSeverity;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.components.ApplicationComponent;
 import com.intellij.openapi.components.PersistentStateComponent;
 import com.intellij.openapi.components.State;
 import com.intellij.openapi.components.Storage;
-import com.intellij.openapi.editor.colors.CodeInsightColors;
-import com.intellij.openapi.editor.colors.TextAttributesKey;
-import com.intellij.openapi.editor.markup.EffectType;
-import com.intellij.openapi.editor.markup.TextAttributes;
-import com.intellij.openapi.project.Project;
-import com.intellij.openapi.project.ProjectManager;
-import com.intellij.openapi.project.ProjectManagerListener;
 import com.intellij.util.xmlb.XmlSerializerUtil;
 import org.intellij.spellChecker.engine.SpellChecker;
 import org.intellij.spellChecker.engine.SpellCheckerFactory;
@@ -39,11 +29,12 @@ import org.intellij.spellChecker.inspections.*;
 import org.intellij.spellChecker.util.Strings;
 import org.jetbrains.annotations.NotNull;
 
-import java.awt.*;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.*;
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 /**
  * Spell checker inspection provider.
@@ -58,19 +49,11 @@ import java.util.List;
                 file = "$APP_CONFIG$/spellchecker.xml"
         )}
 )
-public final class SpellCheckerManager implements ApplicationComponent, InspectionToolProvider, PersistentStateComponent<SpellCheckerManager.State>, ProjectManagerListener {
-    private static final HighlightSeverity SPELLING = new HighlightSeverity("SPELLING", HighlightSeverity.INFO.myVal);
-    private static final TextAttributesKey SPELLING_ATTRIBUTES = TextAttributesKey.createTextAttributesKey("SPELLING_ATTRIBUTES");
+public final class SpellCheckerManager implements ApplicationComponent, InspectionToolProvider, PersistentStateComponent<SpellCheckerManager.State> {
     private static final int MAX_SUGGESTIONS_THRESHOLD = 10;
-
-    private final ProjectManager projectManager;
 
     public static SpellCheckerManager getInstance() {
         return ApplicationManager.getApplication().getComponent(SpellCheckerManager.class);
-    }
-
-    public SpellCheckerManager(ProjectManager projectManager) {
-        this.projectManager = projectManager;
     }
 
     private static final Class[] INSPECTIONS = {
@@ -88,12 +71,6 @@ public final class SpellCheckerManager implements ApplicationComponent, Inspecti
     private final State state = new State();
 
     public void initComponent() {
-        projectManager.addProjectManagerListener(this);
-
-        initSeverity(SeverityRegistrar.getInstance());
-        TextAttributes sta = getSpellingTextAttributes();
-        HighlightDisplayLevel.registerSeverity(SPELLING, sta.getErrorStripeColor());
-
         for (String word : ejectAll(state.IGNORED_WORDS)) {
             ignoreAll(word);
         }
@@ -102,68 +79,18 @@ public final class SpellCheckerManager implements ApplicationComponent, Inspecti
         }
     }
 
-    public void projectOpened(Project project) {
-        initSeverity(SeverityRegistrar.getInstance(project));
-    }
-
-    public boolean canCloseProject(Project project) {
-        return true;
-    }
-
-    public void projectClosed(Project project) {
-        disposeSeverity(SeverityRegistrar.getInstance(project));
-    }
-
-    public void projectClosing(Project project) {
-    }
-
     private HashSet<String> ejectAll(Set<String> from) {
         HashSet<String> words = new HashSet<String>(from);
         from.clear();
         return words;
     }
 
-    private void initSeverity(SeverityRegistrar severityRegistrar) {
-        TextAttributes sta = getSpellingTextAttributes();
-        if (!severityRegistrar.isSeverityValid(SPELLING)) {
-            HighlightInfoType.HighlightInfoTypeImpl hiti = new HighlightInfoType.HighlightInfoTypeImpl(SPELLING, SPELLING_ATTRIBUTES);
-            SeverityRegistrar.SeverityBasedTextAttributes attributes = new SeverityRegistrar.SeverityBasedTextAttributes(sta, hiti);
-
-            List<String> severites = new ArrayList<String>();
-            for (int i = 0; i < severityRegistrar.getSeveritiesCount(); i++) {
-                severites.add(severityRegistrar.getSeverityByIndex(i).myName);
-            }
-
-            severityRegistrar.registerSeverity(attributes, sta.getErrorStripeColor());
-
-            severites.add(SPELLING.myName);
-            severityRegistrar.setOrder(severites);
-        }
-    }
-
-    private void disposeSeverity(SeverityRegistrar severityRegistrar) {
-        severityRegistrar.unregisterSeverity(SPELLING);
-    }
-
-    @NotNull
-    private static TextAttributes getSpellingTextAttributes() {
-        TextAttributes infoAttrs = CodeInsightColors.INFO_ATTRIBUTES.getDefaultAttributes();
-        if (infoAttrs == null) {
-            infoAttrs = new TextAttributes(null, null, new Color(204, 204, 204), EffectType.WAVE_UNDERSCORE, 0);
-            infoAttrs.setErrorStripeColor(new Color(255, 255, 204));
-        }
-        return infoAttrs;
-    }
-
     @NotNull
     public static HighlightDisplayLevel getHighlightDisplayLevel() {
-        HighlightDisplayLevel level = HighlightDisplayLevel.find(SPELLING);
-        return level != null ? level : HighlightDisplayLevel.WARNING;
+        return HighlightDisplayLevel.INFO;
     }
 
     public void disposeComponent() {
-        disposeSeverity(SeverityRegistrar.getInstance());
-        projectManager.removeProjectManagerListener(this);
     }
 
     @NotNull
@@ -188,13 +115,18 @@ public final class SpellCheckerManager implements ApplicationComponent, Inspecti
         return spellChecker;
     }
 
-    public boolean hasProblem(String word) {
-        return !spellChecker.isIgnored(word) && !spellChecker.isCorrect(word);
+    public boolean hasProblem(@NotNull String word) {
+        return !isIgnored(word) && !spellChecker.isCorrect(word);
+    }
+
+    private boolean isIgnored(@NotNull String word) {
+        return spellChecker.isIgnored(word.toLowerCase());
     }
 
     @SuppressWarnings({"unchecked"})
-    public List<String> getSuggestions(String word) {
-        if (!spellChecker.isIgnored(word) && !spellChecker.isCorrect(word)) {
+    @NotNull
+    public List<String> getSuggestions(@NotNull String word) {
+        if (!isIgnored(word) && !spellChecker.isCorrect(word)) {
             List<String> suggestions = spellChecker.getSuggestions(word, MAX_SUGGESTIONS_THRESHOLD);
             if (suggestions.size() != 0) {
                 boolean capitalized = Strings.isCapitalized(word);
@@ -218,7 +150,7 @@ public final class SpellCheckerManager implements ApplicationComponent, Inspecti
      * @param inputStream Dictionary input stream
      * @throws java.io.IOException if dictionary load with problems
      */
-    public void addDictionary(InputStream inputStream) throws IOException {
+    public void addDictionary(@NotNull InputStream inputStream) throws IOException {
         spellChecker.addDictionary(inputStream);
     }
 
