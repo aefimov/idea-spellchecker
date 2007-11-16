867ea680591087da1c3588c6adbf76c2494aa9b0
aefimov.box <aefimov.box@788ae795-663a-0410-9dc1-159affa97516>
2007-11-16 20:51:57 +0000

    Issue 23: Spell checker for mixed languages

--- src/org/intellij/spellChecker/engine/JazzySpellChecker.java
+++ src/org/intellij/spellChecker/engine/JazzySpellChecker.java
@@ -34,7 +34,8 @@ import java.util.*;
  */
 final class JazzySpellChecker implements SpellChecker {
     private final com.swabunga.spell.event.SpellChecker delegate = new com.swabunga.spell.event.SpellChecker();
-    private final Set<SpellDictionaryImpl> dictionaries = new HashSet<SpellDictionaryImpl>();
+    private final Map<SpellDictionaryImpl, Set<Character>> dictionaries = new HashMap<SpellDictionaryImpl, Set<Character>>();
+    private final Set<Character> allowed = new HashSet<Character>();
     private SpellDictionaryImpl userDictionary;
 
     JazzySpellChecker() {
@@ -43,12 +44,22 @@ final class JazzySpellChecker implements SpellChecker {
 
     public void addDictionary(@NotNull InputStream is, @NonNls String encoding, @NotNull Locale locale) throws IOException {
         SpellDictionaryImpl spellDictionary = new SpellDictionaryImpl(new InputStreamReader(is, encoding), locale);
-        dictionaries.add(spellDictionary);
+        Set<Character> indexedChars = spellDictionary.getIndexedChars();
+        dictionaries.put(spellDictionary, indexedChars);
+        allowed.addAll(indexedChars);
         delegate.addDictionary(spellDictionary);
     }
 
     public void addToDictionary(@NotNull String word) {
         delegate.addToDictionary(word);
+        indexWord(word);
+    }
+
+    private void indexWord(String word) {
+        char[] chars = word.toCharArray();
+        for (char c : chars) {
+            allowed.add(c);
+        }
     }
 
     public void ignoreAll(@NotNull String word) {
@@ -56,37 +67,84 @@ final class JazzySpellChecker implements SpellChecker {
     }
 
     public boolean isIgnored(@NotNull String word) {
-        return delegate.isIgnored(word);
+        return !isSomeCharsAllowed(word, allowed) || delegate.isIgnored(word);
     }
 
     public boolean isCorrect(@NotNull String word) {
-        return delegate.isCorrect(word);
+        return !isSomeCharsAllowed(word, allowed) || delegate.isCorrect(word);
+    }
+
+    private boolean isSomeCharsAllowed(String word, Set<Character> index) {
+        char[] chars = word.toCharArray();
+        for (char c : chars) {
+            if (index.contains(c)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean isEntireWordAllowed(String word, Set<Character> index) {
+        char[] chars = word.toCharArray();
+        for (char c : chars) {
+            if (!index.contains(c)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private Set<Character> findDictionaryIndex(String word) {
+        Collection<Set<Character>> indexes = dictionaries.values();
+        for (Set<Character> index : indexes) {
+            if (isEntireWordAllowed(word, index)) {
+                return index;
+            }
+        }
+        return null;
     }
 
     @NotNull
     @SuppressWarnings({"unchecked"})
     public List<String> getSuggestions(@NotNull String word, int threshold) {
         List<Word> words = delegate.getSuggestions(word, threshold);
+        Set<Character> index = findDictionaryIndex(word);
         List<String> strings = new ArrayList<String>(words.size());
         for (Word w : words) {
-            strings.add(w.getWord());
+            String suggestion = w.getWord();
+            if (index == null || isEntireWordAllowed(suggestion, index)) {
+                strings.add(suggestion);
+            }
         }
         return strings;
     }
 
     @NotNull
     public List<String> getVariants(@NotNull String prefix) {
-        List<String> variants = new ArrayList<String>();
-        userDictionary.appendWordsStartsWith(prefix, variants);
-        for (SpellDictionaryImpl dictionary : dictionaries) {
-            dictionary.appendWordsStartsWith(prefix, variants);
+        if (prefix.length() > 0) {
+            List<String> variants = new ArrayList<String>();
+            userDictionary.appendWordsStartsWith(prefix, variants);
+            Set<Character> index = findDictionaryIndex(prefix);
+            if (index != null) {
+                for (SpellDictionaryImpl dictionary : dictionaries.keySet()) {
+                    if (index.equals(dictionaries.get(dictionary))) {
+                        dictionary.appendWordsStartsWith(prefix, variants);
+                    }
+                }
+            }
+            Collections.sort(variants);
+            return variants;
         }
-        Collections.sort(variants);
-        return variants;
+        return Collections.emptyList();
     }
 
     public void reset() {
         delegate.reset();
+        allowed.clear();
+        Collection<Set<Character>> sets = dictionaries.values();
+        for (Set<Character> set : sets) {
+            allowed.addAll(set);
+        }
         setUserDictionary();
     }
 
@@ -109,6 +167,25 @@ final class JazzySpellChecker implements SpellChecker {
         private SpellDictionaryImpl(Reader wordList, Locale locale) throws IOException {
             super(wordList);
             this.locale = locale;
+        }
+
+        @SuppressWarnings({"unchecked"})
+        public Set<Character> getIndexedChars() {
+            Set<Character> index = new HashSet<Character>(64);
+            Collection<List<String>> values = mainDictionary.values();
+            for (List<String> wordList : values) {
+                if (wordList != null) {
+                    for (String word : wordList) {
+                        if (word != null) {
+                            char[] chars = word.toCharArray();
+                            for (char c : chars) {
+                                index.add(c);
+                            }
+                        }
+                    }
+                }
+            }
+            return index;
         }
 
         @SuppressWarnings({"unchecked"})
